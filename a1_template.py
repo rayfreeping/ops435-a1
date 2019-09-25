c0eaeaeee1d483d23023327ef8f7baad1d690108
Raymond Chan <ray.freeping@gmail.com>
2019-09-24 23:32:14 -0400

    add script template

---
+++ a1_template.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+''' put your script level docstring here...
+    you can have more than one line
+'''
+
+import os
+import sys
+
+def after(today):
+    '''
+    put your function level docstring here ...
+    '''
+    ...
+
+    return next_day
+
+def before(today):
+    '''
+    put your function level docstring here ..
+    '''
+    ...
+
+    return previous_day
+
+...
+
+def dbda(date, days):
+    ...
+    setup loop:
+        call after() of before() as appropriate
+    return target_date
+
+if __name__ == "__main__":
+  
+   ... processing command line arguments ...
+   ... call dbda()
+   ...
+   ... output the result
+  
