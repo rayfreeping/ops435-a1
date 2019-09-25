e22cccf03a36b69bd4b658c78cebed6a5dfc018d
Raymond Chan <ray.freeping@gmail.com>
2019-09-24 22:54:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ops435-a1
+# Resource, sample code, and test data
+
+For the detail specification, please go to
+https://wiki.cdot.senecacollege.ca/wiki/OPS435_Python3_Assignment_1
+
+Due date: October 11, 2019 before mid-night
+
---
+++ a1_v1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python3
+'''Python script for OPS435 assignment 1'''
+
+#Author: Raymond Chan
+#Author ID: rchan
+#Date: 2019/09/25
+
+import sys
+first_number = int(sys.argv[1].replace('/',''))
+second_number = int(sys.argv[2])
+result = str(first_number + second_number)
+print(result[0:4]+'/'+result[4:6]+'/'+result[6:])
