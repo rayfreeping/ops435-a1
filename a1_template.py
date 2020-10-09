71ccce4ec5a1f0579ef2607bd73eca749fcf64c0
Raymond Chan <ray.freeping@gmail.com>
2020-10-09 00:23:34 -0400

    Update a1_template.py

--- a1_template.py
+++ a1_template.py
@@ -2,8 +2,18 @@
 ''' template for ops435 assignment 1 script
     put your script level docstring here...
     you can have more than one line
+-----------------------------------------------------------------------
+OPS435 Assignment 1 - Fall 2020
+Program: a1_[Seneca_name].py (replace [Seneca_name] with your Seneca User name)
+Author: "Student Name"
+The python code in this file (a1_[Seneca_name].py) is original work written by
+"Student Name". No code in this file is copied from any other source 
+except those provided by the course instructor, including any person, 
+textbook, or on-line resource. I have not shared this python script 
+with anyone or anything except for submission for grading.  
+I understand that the Academic Honesty Policy will be enforced and 
+violators will be reported and appropriate action will be taken.
 '''
-
 import os
 import sys
 
@@ -15,6 +25,14 @@ def leap_year(obj):
 
     return status
 
+def sanitize(obj1,obj2):
+    '''
+    put your function level docstring here ...
+    '''
+    ...
+    
+    return results
+
 def size_check(obj):
     '''
     put your function level docstring here ..
@@ -23,23 +41,29 @@ def size_check(obj):
 
     return status
 
-...
-
-def value_check(obj1, obj2):
-    ...
+def range_check(obj1, obj2):
+    '''
     put your function level docstring here ..
     '''
     ...
     
     return status
+    
+def usage():    
+    '''
+    put your function level docstring here ...
+    '''
+    ...
+    return status
 
+    
 if __name__ == "__main__":
   
    ... access command line argument  ...
    ... sanitized the input data
    ... check for the size of the input data
    ... extract year, month, day from input data
-   ... validate extracted data item
-   ... convert validate input data to standard format
+   ... perform range check on extracted data item
+   ... convert validated input data to standard format
    ... output the result
   
