8dd37b53320e97baf102ae03916d422831aa4977
Raymond Chan <ray.freeping@gmail.com>
2020-10-10 17:23:03 -0400

    Add python code for the main program for student to translate them into simple english

--- a1_template.py
+++ a1_template.py
@@ -56,14 +56,47 @@ def usage():
     ...
     return status
 
-    
 if __name__ == "__main__":
-  
-   ... access command line argument  ...
-   ... sanitized the input data
-   ... check for the size of the input data
-   ... extract year, month, day from input data
-   ... perform range check on extracted data item
-   ... convert validated input data to standard format
-   ... output the result
-  
+   # step 1
+   if len(sys.argv) != 2:
+      usage()
+      sys.exit()
+   # step 2
+   month_name = ['Jan','Feb','Mar','Apr','May','Jun',
+                 'Jul','Aug','Sep','Oct','Nov','Dec']
+   days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+                    7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+   user_raw_data = sys.argv[1]
+   # step 3
+   allow_chars = '0123456789'
+   dob = sanitize(user_raw_data, allow_chars)
+   print('Sanitized user data:', dob)
+   # setp 4
+   result = size_check(dob,8)
+   if result == False:
+       print("Error 09: wrong data entered")
+       sys.exit()
+   # step 5
+   year = int(dob[0:4])
+   month = int(dob[4:6])
+   day = int(dob[6:])
+   # step 6
+   result = range_check(year,(1900,9999))
+   if result == False:
+       print("Error 10: year out of range, must be 1900 or later")
+       sys.exit()
+   result = range_check(month,(1,12))
+   if result == False:
+       print("Error 02: Wrong month entered")
+       sys.exit()
+   result = leap_year(year)
+   if result == True:
+       days_in_month[2] = 29
+   result = range_check(day, (1, days_in_month[month]))
+   if result == False:
+       print("Error 03: wrong day entered")
+       sys.exit()
+   # step 7
+   new_dob = str(month_name[month - 1])+' '+ str(day)+', '+str(year)
+   # step 8
+   print("Your date of birth is:", new_dob)  
